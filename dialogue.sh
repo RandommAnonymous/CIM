cc9c774daa11111fc24097fcbe7565e4086fbb71
anonymous <anonymous>
2024-02-17 19:28:23 +0800

    CIM

--- dialogue.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash/
-python -m torch.distributed.run --nproc_per_node=1 eval_person_retrieval.py \
---output_dir 'output/Retrieval_msrvtt' --task 're_icfg' \
---checkpoint './output/icfg/checkpoint_best.pth' 
--- train_retrieval.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python3 run.py \
---task "itr_icfg" \
---dist "gpu0" \
---output_dir "your_log_path" \
---checkpoint "your_ckpt_path"
